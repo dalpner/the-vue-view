dff558787c0c25c9521b26c9bd6279c496998e63
Dominic Leipner Alvarez <dominic.leipner.alvarez@cancom.de>
2023-08-03 16:55:41 +0200

    try added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,26 @@
 </template>
 
 <script>
+
+
 import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
-    
     NavBar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
